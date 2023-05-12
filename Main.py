06c84c7c7e9b3ac27c4a17037d1fb5c1631609f0
Samuel M <s.montanez@uniandes.edu.co>
2023-05-12 11:47:34 -0500

    SaveFig

--- Main.py
+++ Main.py
@@ -3,14 +3,15 @@ import sys
 import matplotlib.pyplot as plt
 import time
 
+sys.setrecursionlimit(10**9)
+
 def printMenu():
     print("Bienvenido")
-    print("1- Queue")
-    print("2- Stack")
-    print("3- DisjointSet LinkedList")
-    print("4- Union de Queue")
+    print("1- Forest LL Phase Transition")
+    print("2- Forest LL Random")
+    print("3- Forest Phase Transition")
+    print("4- Forest Random")
     print("5- Warehouse")
-    print("6- DisjointSet Forest")
     print("0- Salir")
 
 def initialP():
@@ -21,24 +22,41 @@ def initialP():
         Verifier = True
     except ValueError:
         pass
-    if Verifier:
+    if Verifier and condition<=3000:
         return condition
     else:   
-        print('Ingrese solo valor numérico.')
+        print('Ingrese solo valor numérico. No mayor a 3000.')
 
-def showG(x,y,xlabel,ylabel):
-    plt.plot(x,y,'r-')
+def showG(x,y,xlabel,ylabel,color='r-'):
+    plt.plot(x,y,color)
     font1 = {'family':'serif','color':'green','size':15}
     plt.xlabel(xlabel, fontdict = font1)
     plt.ylabel(ylabel, fontdict = font1)
     plt.show()
 
-def saveData(y,folder,size):
+def showMultiG(x,y,y2,xlabel,c1='r-',c2='b--',l1=None,l2=None):
+    fig, ax = plt.subplots()
+    ax.plot(x, y , c1, label=l1)
+    ax.plot(x, y2, c2, label=l2)
+    ax.legend(loc='upper right')
+    font1 = {'family':'serif','color':'green','size':15}
+    plt.xlabel(xlabel, fontdict = font1)
+    plt.show()
+
+def saveData(y,folder,size=None):
     date = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
     with open('./Data/'+folder+'/'+date+'_'+str(size)+'.txt', 'w') as f:
         for line in y:
             f.write(str(line))
             f.write('\n')
+
+def saveFig(folder,size=None):
+    date = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
+    if size:
+        plt.savefig('./Data/'+folder+'/'+date+'_'+size+'.png')
+    else:
+        plt.savefig('./Data/'+folder+'/'+date+'.png')
+
 
 while True:
     printMenu()
@@ -46,29 +64,38 @@ while True:
     if int(inputs[0]) == 1:
         condition = initialP()
         if condition:
-            x,y = Model.queueF(condition)
-            showG(x,y,'','')
-            saveData(y,'Queue',condition)
+            x,y = Model.ForestLLPT(condition)
+            showG(x,y,'make/join','<size>')
+            saveFig('ForestLL',size=condition)
+            #saveData(y,'ForestLL',condition)
 
     elif int(inputs[0]) == 2:
-        condition = initialP()
-        if condition:
-            x,y = Model.stackF(condition)
-            showG(x,y,'','')
-            saveData(y,'Stack',condition)
+        x,y = Model.ForestLLR()
+        showG(x,y,'make/join','<size>')
+        saveFig('ForestLL')
+        #saveData(y,'ForestLL')  
 
     elif int(inputs[0]) == 3:
         condition = initialP()
         if condition:
-            x,y = Model.disjointSet_LinkedListF(condition)
-            showG(x,y,'','')
-            saveData(y,'DisjointSet',condition)
+            x,y,y2 = Model.ForestPT(condition)
+            showMultiG(x,y,y2,'make/join',l1='<size>',l2='<degree>')
+            saveFig('Forest',size=condition)
+            #saveData(y,'Forest',condition)
 
     elif int(inputs[0]) == 4:
-        None
+        x,y,y2 = Model.ForestR()
+        showMultiG(x,y,y2,'make/join',l1='<size>',l2='<degree>')
+        saveFig('Forest')
+        #saveData(y,'Forest')
 
     elif int(inputs[0]) == 5:
-        None
+        condition = initialP()
+        if condition:
+            x,y = Model.Warehouse(condition)
+            showG(x,y,'pop/push','<height>')
+            saveFig('Warehouse',size=condition)
+            #saveData(y,'Warehouse',condition)
 
     elif int(inputs[0]) == 0:
         sys.exit(0)
