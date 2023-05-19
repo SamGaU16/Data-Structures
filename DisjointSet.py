3947fd13fa6695d4369818f0905b2069eab4c57c
Samuel M <s.montanez@uniandes.edu.co>
2023-05-18 19:51:39 -0500

    Fix DisjointSets

--- DisjointSet.py
+++ DisjointSet.py
@@ -1,39 +1,77 @@
-class Node:
+from SingleLinked import LinkedList
+
+class Leaf:
     def __init__(self, data=None):
         self.data = data
         self.parent = None
+        self.children = []
         self.rank = 0
-        self.disjoint = None
         self.degree = 0
 
-class DisjointSet:
-    def __init__(self, data):
-        self.nodes = []
-        new_node = Node(data)
+class Tree:
+    def __init__(self, data=None):
+        new_node = Leaf(data)
         new_node.parent = new_node
-        self.nodes.append(new_node)
-        new_node.disjoint = self
+        
+        self.root = new_node
+        self.nodes = 1
     
-    def find(self, pos):
-        node = self.nodes[pos]
-        if node != node.parent:
-            node.parent = self.find(pos+1)
-        return node.parent
-    
-    def union(self, set2):
-        representative_N1 = self.find(0) 
-        representative_N2 = set2.find(0)
-        for node in set2.nodes:
-            self.nodes.append(node)
-        self.link(representative_N1,representative_N2)
+    def degrees(self):
+        return self.degreeRecursive(self.root, 0)
 
-    def link(self, Node1: Node, Node2: Node):
+    def degreeRecursive(self, Node: Leaf, count: int):
+        count += Node.degree
+        if len(Node.children) == 0:
+            return count
+        for leaf in Node.children:
+            count = self.degreeRecursive(leaf, count)
+        return count
+
+
+class DisjointSet:
+    def __init__(self):
+        self.forest = LinkedList()
+
+    def makeSet(self,data=None):
+        self.forest.appendLast(Tree(data))
+
+    def find(self, pos: int):
+        return self.forest.getNode(pos).data
+    
+    def union(self, pos1: int, pos2: int):
+        if pos1 < 1:
+            return
+        set1 = self.find(pos1) 
+        set2 = self.find(pos2)
+        new_representative = self.link(set1.root, set2.root)
+        set1.root = new_representative
+        set1.nodes += set2.nodes
+        self.forest.unlink(pos2)
+        
+    def link(self, Node1: Leaf, Node2: Leaf):
         Node1.degree +=1
         Node2.degree +=1
         if Node1.rank > Node2.rank:
             Node2.parent = Node1
+            Node1.children.append(Node2)
+            return Node1
         else:
             Node1.parent = Node2
+            Node2.children.append(Node1)
             if Node1.rank == Node2.rank:
                 Node2.rank += 1
-
+            return Node2
+    
+    def size(self):
+        return self.forest.size
+    
+    def leafsInfo(self):
+        sizeTree = 0
+        degreesLeafs = 0
+        Node = self.forest.getNode(1)
+        while Node is not None:
+            Tree = Node.data
+            sizeTree += Tree.nodes
+            degreesLeafs += Tree.degrees()
+            Node = Node.next
+        return sizeTree, degreesLeafs
--- DisjointSet_LinkedList.py
+++ DisjointSet_LinkedList.py
@@ -1,27 +1,55 @@
-class Node:
-    def __init__(self, data, next=None, disjoint=None):
+from SingleLinked import LinkedList
+
+class SNode:
+    def __init__(self, data, next=None, lst=None):
         self.data = data
         self.next = next
-        self.disjoint = disjoint
+        self.lst = lst
 
-class DisjointSet_L:
+class SList:
     def __init__(self, data):
-        new_node = Node(data)
+        new_node = SNode(data)
         self.head = new_node
         self.tail = new_node
         self.size = 1
-        new_node.disjoint = self
-    
-    def union(self, set2):
-        union_node = self.tail
-        union_node.next = set2.head                                                                                                                                                                                     
-        last_node = self.replace_set(set2.head)
-        self.tail = last_node
+        new_node.lst = self
+        
+class DisjointSet_L:
+    def __init__(self):
+        self.forest = LinkedList()
 
-    def replace_set(self, node: Node):
-        node.disjoint = self
-        self.size += 1
+    def makeSet(self,data=None):
+        self.forest.appendLast(SList(data))
+
+    def find(self, pos: int):
+        return self.forest.getNode(pos).data
+    
+    def union(self, pos1: int, pos2: int):
+        if pos1 < 1:
+            return
+        set1 = self.find(pos1) 
+        set2 = self.find(pos2)
+        union_node = set1.tail
+        union_node.next = set2.head
+        last_node = self.replace_set(set1, set2.head)
+        set1.tail = last_node
+        self.forest.unlink(pos2)
+
+    def replace_set(self, set: SList, node: SNode):
+        node.lst = set
+        set.size += 1
         if node.next: 
-            return self.replace_set(node.next)
+            return self.replace_set(set, node.next)
         else:
             return node
+    
+    def size(self):
+        return self.forest.size
+    
+    def nodes(self):
+        nodes = 0
+        Node = self.forest.getNode(1)
+        while Node is not None:
+            nodes += Node.data.size
+            Node = Node.next
+        return nodes
