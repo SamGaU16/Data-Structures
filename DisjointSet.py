0ecb04cddb09ef5a229c2e9ebebeab9302893bb7
Samuel M <s.montanez@uniandes.edu.co>
2023-05-28 19:00:35 -0500

    Fix nombre leafs

--- DisjointSet.py
+++ DisjointSet.py
@@ -14,7 +14,7 @@ class Tree:
         new_node.parent = new_node
         
         self.root = new_node
-        self.nodes = 1
+        self.size = 1
     
     def degrees(self):
         return self.degreeRecursive(self.root, 0)
@@ -31,21 +31,25 @@ class Tree:
 class DisjointSet:
     def __init__(self):
         self.forest = LinkedList()
+        self.leafs = 0
+        self.degrees = 0
 
     def makeSet(self,data=None):
         self.forest.appendLast(Tree(data))
+        self.leafs += 1
 
     def find(self, pos: int):
         return self.forest.getNode(pos).data
     
     def union(self, pos1: int, pos2: int):
+        self.degrees += 2
         if pos1 < 1:
             return
         set1 = self.find(pos1) 
         set2 = self.find(pos2)
         new_representative = self.link(set1.root, set2.root)
         set1.root = new_representative
-        set1.nodes += set2.nodes
+        set1.size += set2.size
         self.forest.unlink(pos2)
         
     def link(self, Node1: Leaf, Node2: Leaf):
@@ -66,12 +70,4 @@ class DisjointSet:
         return self.forest.size
     
     def leafsInfo(self):
-        sizeTree = 0
-        degreesLeafs = 0
-        Node = self.forest.getNode(1)
-        while Node is not None:
-            Tree = Node.data
-            sizeTree += Tree.nodes
-            degreesLeafs += Tree.degrees()
-            Node = Node.next
-        return sizeTree, degreesLeafs
+        return self.leafs, self.degrees
